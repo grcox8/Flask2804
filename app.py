d47e2f38869bdc9e5523ca1ee41f1fa31eedeb5b
grcox8 <gregoriotinoco08@gmail.com>
2025-04-28 15:43:51 -0300

    Definicion de 2 rutas simples.

--- app.py
+++ app.py
@@ -5,3 +5,11 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/hl")
+def hasta_luego():
+    return "<p>Hasta luego!</p>"
+
+@app.route("/bv")
+def bienvenido():
+    return "<p>Bienvenido!</p>"
